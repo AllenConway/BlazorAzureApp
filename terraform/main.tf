ce89dc6ecca47553ef0aa18b7a8a66c4420d4eb2
Allen Conway <allen.conway@xebia.com>
2024-11-15 18:55:37 -0500

    Refactor Terraform configuration to simplify resource lookups and enhance deployment workflow for Azure Web App

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,70 +12,24 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-# All data source lookups first
-data "azurerm_subscription" "current" {}
-
-data "azurerm_resource_group" "existing" {
-  count = try(data.azurerm_subscription.current.id != "", false) ? 1 : 0
-  name  = var.resource_group_name
-}
-
-# First resource creation - Resource Group
 resource "azurerm_resource_group" "rg" {
-  count    = try(data.azurerm_resource_group.existing[0].id != "", false) ? 0 : 1
   name     = var.resource_group_name
   location = var.location
 }
 
-# Resource group local for use by other resources
-locals {
-  resource_group = try(data.azurerm_resource_group.existing[0], azurerm_resource_group.rg[0])
-}
-
-# Service Plan lookups and creation
-data "azurerm_service_plan" "existing" {
-  count               = try(local.resource_group.name != "", false) ? 1 : 0
-  name                = var.app_service_plan_name
-  resource_group_name = local.resource_group.name
-  depends_on         = [azurerm_resource_group.rg]
-}
-
-locals {
-  service_plan_exists = try(data.azurerm_service_plan.existing[0].id != "", false)
-}
-
 resource "azurerm_service_plan" "asp" {
-  count               = local.service_plan_exists ? 0 : 1
   name                = var.app_service_plan_name
-  location            = local.resource_group.location
-  resource_group_name = local.resource_group.name
-  os_type            = "Linux"
-  sku_name           = "F1"
-  depends_on         = [local.resource_group]
-}
-
-locals {
-  current_service_plan_id = local.service_plan_exists ? data.azurerm_service_plan.existing[0].id : azurerm_service_plan.asp[0].id
-}
-
-# Web App lookups and creation
-data "azurerm_linux_web_app" "existing" {
-  count               = try(local.resource_group.name != "", false) ? 1 : 0
-  name                = var.app_service_name
-  resource_group_name = local.resource_group.name
-  depends_on         = [local.resource_group]
-}
-
-locals {
-  web_app_exists = try(data.azurerm_linux_web_app.existing[0].id != "", false)
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "app" {
-  count               = local.web_app_exists ? 0 : 1
   name                = var.app_service_name
-  location            = local.resource_group.location
-  resource_group_name = local.resource_group.name
-  service_plan_id     = local.current_service_plan_id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     always_on = false
@@ -114,13 +68,7 @@ resource "azurerm_linux_web_app" "app" {
   }
 
   depends_on = [
-    local.resource_group,
+    azurerm_resource_group.rg,
     azurerm_service_plan.asp
   ]
-}
-
-# Final output locals at the end
-locals {
-  service_plan = local.service_plan_exists ? data.azurerm_service_plan.existing[0] : (length(azurerm_service_plan.asp) > 0 ? azurerm_service_plan.asp[0] : null)
-  web_app     = local.web_app_exists ? data.azurerm_linux_web_app.existing[0] : (length(azurerm_linux_web_app.app) > 0 ? azurerm_linux_web_app.app[0] : null)
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
 output "app_service_default_hostname" {
-  value = local.web_app.default_hostname
+  value = azurerm_linux_web_app.app.default_hostname
 }
 
 output "app_service_name" {
-  value = var.app_service_name
+  value = azurerm_linux_web_app.app.name
 }
 
 output "app_service_plan_name" {
-  value = var.app_service_plan_name
+  value = azurerm_service_plan.asp.name
 }
 
 output "resource_group_name" {
-  value = var.resource_group_name
+  value = azurerm_resource_group.rg.name
 }
