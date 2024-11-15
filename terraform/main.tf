2945087c38b6988a2b6f96e71719ab39c07bd583
Allen Conway <allen.conway@xebia.com>
2024-11-15 14:26:48 -0500

    Refactor Terraform configuration to simplify resource checks and update local references

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,32 +18,38 @@ data "azurerm_resource_group" "existing" {
   name  = var.resource_group_name
 }
 
-data "azurerm_service_plan" "existing" {
-  count               = can(local.resource_group.id) ? 1 : 0
-  name                = var.app_service_plan_name
-  resource_group_name = local.resource_group.name
-}
-
-data "azurerm_linux_web_app" "existing" {
-  count               = can(local.resource_group.id) ? 1 : 0
-  name                = var.app_service_name
-  resource_group_name = local.resource_group.name
-}
-
 data "azurerm_subscription" "current" {}
 
-# Update resource group creation condition
+# Resource group creation condition
 resource "azurerm_resource_group" "rg" {
   count    = can(data.azurerm_resource_group.existing[0].id) ? 0 : 1  # Create only if doesn't exist
   name     = var.resource_group_name
   location = var.location
 }
 
-# Simplified references to resources
+# Simplified reference to resource group
 locals {
   resource_group = try(data.azurerm_resource_group.existing[0], azurerm_resource_group.rg[0])
-  service_plan   = try(data.azurerm_service_plan.existing[0], azurerm_service_plan.asp[0])
-  web_app       = try(data.azurerm_linux_web_app.existing[0], azurerm_linux_web_app.app[0])
+}
+
+# Check for existing service plan only if resource group exists
+data "azurerm_service_plan" "existing" {
+  count               = can(data.azurerm_resource_group.existing[0].id) ? 1 : 0
+  name                = var.app_service_plan_name
+  resource_group_name = var.resource_group_name
+}
+
+# Check for existing web app only if service plan exists
+data "azurerm_linux_web_app" "existing" {
+  count               = can(data.azurerm_service_plan.existing[0].id) ? 1 : 0
+  name                = var.app_service_name
+  resource_group_name = var.resource_group_name
+}
+
+# Update local references
+locals {
+  service_plan = try(data.azurerm_service_plan.existing[0], azurerm_service_plan.asp[0])
+  web_app     = try(data.azurerm_linux_web_app.existing[0], azurerm_linux_web_app.app[0])
 }
 
 # App service plan using local reference
