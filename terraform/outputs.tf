6bcad2a597030aa13d0ea6ccfadd466f3f10bbbc
Allen Conway <allen.conway@xebia.com>
2024-11-15 13:30:04 -0500

    Updated Terraform to ensure existing resources in Azure are acceptable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,14 @@ output "app_service_default_hostname" {
   value = azurerm_linux_web_app.app.default_hostname
 }
 
+output "app_service_name" {
+  value = var.app_service_name
+}
+
+output "app_service_plan_name" {
+  value = var.app_service_plan_name
+}
+
 output "resource_group_name" {
   value = var.resource_group_name
 }
