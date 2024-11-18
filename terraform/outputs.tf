d693a693a8920cfa420fca9cefdaf51dff204a9e
Allen Conway <allen.conway@xebia.com>
2024-11-17 22:51:16 -0500

    Refactor output values in Terraform configuration to use local variables for improved clarity and consistency

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,20 @@
 output "app_service_default_hostname" {
-  value = azurerm_linux_web_app.app.default_hostname
+  value = local.web_app.default_hostname
 }
 
 output "app_service_name" {
-  value       = azurerm_linux_web_app.app.name
+  value       = local.web_app.name
   description = "The name of the web app"
 }
 
 output "app_service_plan_name" {
-  value = azurerm_service_plan.asp.name
+  value = var.app_service_plan_name
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = local.resource_group.name
 }
 
 output "app_service_plan_id" {
-  value = azurerm_service_plan.asp.id
+  value = local.current_service_plan_id
 }
